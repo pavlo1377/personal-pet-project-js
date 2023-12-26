c70af8c15a23ffc2c85bbdbfb2867a3f4aebd80d
pavlo antohiv <pavlo.antohiv@gmail.com>
2023-12-26 15:19:33 +0200

    Final commit

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let startTimer;
 
 start.addEventListener("click", function () {
   if (startTimer === undefined) {
-    startTimer = setInterval(timer, 1);
+    startTimer = setInterval(timer, 1000);
   } else {
     alert("Timer is already running");
   }
